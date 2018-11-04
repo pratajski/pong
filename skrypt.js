93197355c9dc151458839729b70f6c572b7eeb8a
pratajski <p.ratajski@gmail.com>
2018-11-04 11:11:30 +0100

    obiektowosc

--- skrypt.js
+++ skrypt.js
@@ -6,78 +6,100 @@ function start() {
     
 }
 
+ball = {
+x: 320,
+y: 240,
+dx: 0,  
+dy: 0,
+    //przesun pilke
+move: function() {
+    this.x = this.x + this.dx;
+    this.y = this.y + this.dy;
+}
+}
 
+/*
 x = 320;
 y = 240;
 dx = 0;  
 dy = 0;
-
+*/
 losdx = Math.random()*10;
 losdy = Math.random()*10; 
 
 if (losdx >= 5) {
-    dx = 0.8;
+    ball.dx = 0.8;
 }else {
-    dx = -0.8;
+    ball.dx = -0.8;
 }
 
 if (losdy >= 5) {
-    dy = 0.8;
+    ball.dy = 0.8;
 }else {
-    dy = -0.8;
+    ball.dy = -0.8;
 }
 
 //pad1
-p1y = 215;
-p1dy = 0;
+//p1y = 215;
+//p1dy = 0;
+
+pad1 = {
+    y: 215,
+    dy: 0
+}
 
 //pad2
-p2y = 215;
-p2dy = 0;
+//p2y = 215;
+//p2dy = 0;
+
+pad2 = {
+    y: 215,
+    dy: 0
+}
 
 function drawBall() {
-    x = x + dx;
-    y = y + dy;
-    
+    //x = x + dx;
+    //y = y + dy;
+    ball.move();
     //dolna krawedz
-    if (y>475) {
-        dy = -dy;
-        y = 475;
+    if (ball.y>475) {
+        ball.dy = -ball.dy;
+        ball.y = 475;
         audio = document.getElementById("wall");
         audio.play();
     }
     
     //prawa krawedz
-    if ( x > 635) {
-        dx = -dx;
-        x = 635;
+    if ( ball.x > 635) {
+        ball.dx = -ball.dx;
+        ball.x = 635;
         audio = document.getElementById("punkt");
         audio.play();
     }
     
     //górna krawędź
-    if (y < 5) {
-        dy = -dy;
-        y = 5;
+    if (ball.y < 5) {
+        ball.dy = -ball.dy;
+        ball.y = 5;
         audio = document.getElementById("wall");
         audio.play();
     }
     
     //lewa krawedz
-    if ( x < 5) {
-        dx = -dx;
-        x = 5;
+    if ( ball.x < 5) {
+        ball.dx = -ball.dx;
+        ball.x = 5;
         audio = document.getElementById("punkt");
         audio.play();
     }
     
     //lewa paletka
-    if (dx < 0) {
-        if ((x > 12) && (x < 15)) {
+    if (ball.dx < 0) {
+        if ((ball.x > 12) && (ball.x < 15)) {
             
-            if ((y>=p1y) && (y <= p1y+50)) {
-                dx = -dx;
-                x = 15;
+            if ((ball.y>=pad1.y) && (ball.y <= pad1.y+50)) {
+                ball.dx = -ball.dx;
+                ball.x = 15;
                 audio = document.getElementById("paletka");
                 audio.play();
             }            
@@ -85,28 +107,28 @@ function drawBall() {
     }
     //prawa paletka
     
-    if (dx > 0) {
-        if ((x >= 625) && (x < 628)) {
+    if (ball.dx > 0) {
+        if ((ball.x >= 625) && (ball.x < 628)) {
             
-            if ((y>=p2y) && (y <= p2y+50)) {
-                dx = -dx;
-                x = 625;
+            if ((ball.y>=pad2.y) && (ball.y <= pad2.y+50)) {
+                ball.dx = -ball.dx;
+                ball.x = 625;
                 audio = document.getElementById("paletka");
                 audio.play();
             }            
         }
     }
     
-    wygrana = 20;
-    if ((x<=5) ){
+    wygrana = 10;
+    if ((ball.x<=5) ){
         //alert("Punkt zdobywa gracz 2");
         scoreRight++;
-        x=320;
-        y=240;
-        p1y = 215;
-        p1dy = 0;
-        p2y = 215;
-        p2dy = 0;
+        ball.x=320;
+        ball.y=240;
+        pad1.y = 215;
+        pad1.dy = 0;
+        pad2.y = 215;
+        pad2.dy = 0;
         //dx = -dx;
         if (scoreRight>=wygrana) {
             
@@ -116,15 +138,15 @@ function drawBall() {
         }
         }
     
-    if (x>=635) {
+    if (ball.x>=635) {
         //alert("Punkt zdobywa gracz 1");
         scoreLeft++;
-        x=320;
-        y=240;
-        p1y = 215;
-        p1dy = 0;
-        p2y = 215;
-        p2dy = 0;
+        ball.x=320;
+        ball.y=240;
+        pad1.y = 215;
+        pad1.dy = 0;
+        pad2.y = 215;
+        pad2.dy = 0;
        // dx = -dx;
         if (scoreLeft>=wygrana) {
            
@@ -139,28 +161,28 @@ function drawBall() {
     ctx = canvas.getContext("2d");
     ctx.beginPath();
     ctx.fillStyle = "white";
-    ctx.arc(x,y,5,0,2*Math.PI); //pozycja poczatkowa, promien,  kąt podany w radianach
+    ctx.arc(ball.x,ball.y,5,0,2*Math.PI); //pozycja poczatkowa, promien,  kąt podany w radianach
     ctx.fill();
 }
 
 
 function drawPad() {
-    p1y = p1y + p1dy;
-    if (p1y > 430) {
-        p1y = 430;
+    pad1.y = pad1.y + pad1.dy;
+    if (pad1.y > 430) {
+        pad1.y = 430;
     }
     
-    if (p1y < 0) {
-        p1y = 0;
+    if (pad1.y < 0) {
+        pad1.y = 0;
     }
     
-    p2y = p2y + p2dy;
-    if (p2y > 430) {
-        p2y = 430;
+    pad2.y = pad2.y + pad2.dy;
+    if (pad2.y > 430) {
+        pad2.y = 430;
     }
     
-    if (p2y < 0) {
-        p2y = 0;
+    if (pad2.y < 0) {
+        pad2.y = 0;
     }
     
     
@@ -168,8 +190,8 @@ function drawPad() {
     ctx = canvas.getContext("2d");
     ctx.beginPath();
     ctx.fillStyle = "white";
-    ctx.fillRect(5,p1y,5,50);
-    ctx.fillRect(630,p2y,5,50);
+    ctx.fillRect(5,pad1.y,5,50);
+    ctx.fillRect(630,pad2.y,5,50);
     
 }
 
@@ -195,40 +217,39 @@ function printScore() {
 }
 
 
-
 function keyDown(event) {
     console.log(event);
     if (event.keyCode == 90) { 
-        p1dy = 1;
+        pad1.dy = 1;
     }
     if (event.keyCode == 65) { 
-        p1dy = -1;
+        pad1.dy = -1;
     }
     
     if (event.keyCode == 38) { 
-        p2dy = -1;
+        pad2.dy = -1;
     }
     
     if (event.keyCode == 40) { 
-        p2dy = 1;
+        pad2.dy = 1;
     }
     
 }
 
 function keyUp(event) {
     if (event.keyCode == 90) { 
-        p1dy = 0;
+        pad1.dy = 0;
     }
     if (event.keyCode == 65) { 
-        p1dy = 0;
+        pad1.dy = 0;
     }
     
     if (event.keyCode == 38) { 
-        p2dy = 0;
+        pad2.dy = 0;
     }
     
     if (event.keyCode == 40) { 
-        p2dy = 0;
+        pad2.dy = 0;
     }
 }
 
